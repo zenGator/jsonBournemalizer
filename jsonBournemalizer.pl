5fba0bb629d62b281fb7226738cc4de8a3ee7ba6
zG <22377978+zenGator@users.noreply.github.com>
2023-06-02 21:39:38 -0400

    version 2:  use an array instead of double reading
    still need to loop through twice, but this time we'll store each record/object as an array item and parse (first) a copy of each (to build master field name list) and then we can start back at $records[0] and parse each array item directly; we still write as a stream

--- jsonBournemalizer.pl
+++ jsonBournemalizer.pl
@@ -22,6 +22,7 @@ sub usage;
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # switches followed by a : expect an argument
 # see usage() below for explanation of switches
+my $version=0.2;
 my $commandname=$0=~ s/^.*\///r;  #let's know our own name
 my %opt=();
 getopts('dhi:o:l:', \%opt) or usage();		
@@ -29,7 +30,9 @@ getopts('dhi:o:l:', \%opt) or usage();
 my $debugging=TRUE if $opt{d};
 usage () if ( $opt{h} ) ;
 
-#piping /dev/stdin is not an option; piping stdout|stderr (or redirect if specified) is an option
+#ToDo:  consider implementing piping
+#ToDo:  determine if infile is provided as bare argument; how can we handle that?
+#piping /dev/stdin may be possible; piping stdout|stderr (or redirect if specified) is an option
 open(my $inFH, '<:encoding(UTF-8)', $opt{i}) or die "Could not open file '$opt{i}' $!";
 
 my $outFH=*STDOUT;
@@ -49,49 +52,53 @@ if ($opt{l}) {
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #declare variables here
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
-my $lineCount=0;  #primary counter, the line we are currently processing
+my $lineCount=0;	#primary counter, the line we are currently processing
+my $rcdnum=0;		#primary counter, the line/record we are currently processing
 my %allFields;
 my %outBuff;
 my $starttime= strftime ("%Y.%m.%dT%H:%M:%SZ", gmtime time);
+my @records;		#the main array into which we'll stuff the file
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #main
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 printf $logFH "started at %s\n",$starttime if $debugging;	#only do this if desired/requested
 
-#ToDo:  split the raw file into lines using something like this:
-#		sed 's/^\[//;s/},{/}\n{/g' (also drops opening [
-# 		but would then need to build an array and iterate through
-# something like this:
-#	while (my $row=<>){
-#		chomp $row;
-#		@records=split(/},{/,$row);
-#		$rc++; 
-#		printf "record count: %d\n",$#records+1;
-#		printf "%d: %s\n\tfirst: %s, last: %s\n",$lineCount,$row,$records[0], $records[$#records];
-#		}
-# then for (@records) instead of while (my $row = <$inFH>)
-
 #take a pass through entire file to find all the field names
 while (my $row = <$inFH>) {		#get a line
 	chomp $row;					#discard trailing newline char
-    $lineCount++;  						# increment line counter
-	if ($row !~/^{.*}\]?$/){
-		printf $outFH "\n\n%d: %s\n\n", $lineCount, $row;	
-		die "Doesn't begin/end with curley braces.\n"; 	
-		}
-#ToDo:  Test first three chars:  we expect [{"
-	$row =~ s/^{"(.*)}\]?$/$1/;  #strip the opening quote because the parsing will drop opening quote for each name/field; drop the closing brace (& bracket if this is the last row)
+    $lineCount++;  						#increment line counter
+	die "extra data after expected end of Json array" if $lineCount>1; 	#there should only be a single line
+	@records=split(/},{/,$row);			#break the array into single-object elements
+
+#let's make sure the file begins and ends as expected, with [{"<stuff>"]}
+	die "unexpected beginning" if $records[0]=~ s/^\[{// != 1;			
+	die "unexpected ending" if $records[$#records] =~ s/}\]$// != 1; 
+	
+	printf $logFH "record count: %d\n",$#records+1 if $debugging;
+	printf $logFH "%d lines\n\nfirst:\n%s\n\nsecond:\n%s\n\nlast:\n%s\n",$lineCount,$records[0],$records[1],$records[$#records] if $debugging;
+	
+	}			#end of the first major while loop
+
+	
+#we just took care of this	
+#	if ($row !~/^{.*}\]?$/){
+#		printf $outFH "\n\n%d: %s\n\n", $lineCount, $row;	
+#		die "Doesn't begin/end with curley braces.\n"; 	
+#		}
+
+for my $record (@records) {
+	$rcdnum++;
+	$row=$record;		#need a copy of the record/row/line/object for working
+	die "first name should be quoted" if $row =~ s/^"// != 1;	#need to strip the opening quote because the parsing will drop opening quote for each name/field
 	while ($row) {		#so long as there's data remaining on the row, do this
-#printf $logFH "%s\n", $row;
 		$row =~ s/^([^"]*)":("[^"]*"|[0-9.]*|\[[^]]*\]|\{[^}]*\}|(true|false|null))(,"|$)//;
 #ToDo:  put the pattern into a variable that can be used both here and below
-#printf $outFH "1: %s\t 2: %s\n", $1, $2;
 		my $newFN=$1;			#$newFN is newly-discovered field name
-#printf $outFH "%s\n", $newFN;
 		$allFields{$newFN}++;	#increment the value for the key with this field/name
 		}		#end processing the record/row/object
-	}		#end of the first major while loop
+	}
+	printf $logFH "%d records processed\n",$rcdnum if $debugging;
 
 #at this point $allFields is a master list (and template) containing all the names/fields in the objects that make up the Json array
 	
@@ -100,21 +107,17 @@ while (my $row = <$inFH>) {		#get a line
 		printf $logFH "%24s: %s\n",$key,$allFields{$key} if $debugging; #shows occurences of each field across all records, but shown only if desired/requested
 		$outBuff{$key}="null";	#outbuff serves as template
 #ToDo:  can probably just reuse allFields as template
-#		printf $outFH "%s: %s\n",$key,$outBuff{$key}; 	#dbg: template values=null	
 		printf $outFH "%s\t",$key;		#generates the header row for the output file
 		}
 	printf $outFH "\n";		#close off the header
 
-#go back to the top; we won't need to do this after changing to using an in-memory array
-seek $inFH, 0, 0;
-$lineCount=0;		#need to reset the line counter
+$rcdnum=0;		#need to reset
 
-while (my $row = <$inFH>) {		#as above, cycle through every line/object/record
-	#my %currLine = ();		# don't seem to be using this
+for my $row (@records) {	#we should be able to just restart, and this time we can act on the rows directly
     #get a line
 	chomp $row;		#drop trailing newline chars
-    $lineCount++;  # increment line counter
-	$row =~ s/^{"(.*)}\]?$/$1/;  #need to strip the opening quote and final brace/bracket
+    $rcdnum++;  # increment line counter
+	$row =~ s/^"//;
 	while ($row) {		#now processing the row/object/record; loop so long as there's still more to process
 		$row =~ s/^([^"]*)":("[^"]*"|[0-9.]*|\[[^]]*\]|\{[^}]*\}|(true|false|null))(,"|$)//;  #ToDo:  the number RE could be more specific, e.g., [-+]?[0-9]+\.?[0-9]*; what's more, Json actually can have exponential notation, so . . . there's that
 		my $fieldName=$1;			#popping the fieldname from the above substitution
@@ -122,21 +125,16 @@ while (my $row = <$inFH>) {		#as above, cycle through every line/object/record
 		$outBuff{$fieldName} = $newVal;	#build a hash table for this line
 		}									#finished processing this line/record/object
 	foreach my $key (sort keys %outBuff){  #need to sort to keep everything aligned
-#		printf $outFH "%s: %s\n",$key,$allFields{$key}; 	#dbg
-#		printf $outFH "%s: %s\n",$key,$outBuff{$key}; 	#dbg		
 		printf $outFH "%s\t",$outBuff{$key};	#write output
 		$outBuff{$key}="null";					#set each field to null; should be synched with usage in first loop
 		}										#finished writing the output record
 	printf $outFH "\n";							#close of the record with a newline
 	}									#loop back for another line if available
 
-
-
 my $endtime= strftime ("%Y.%m.%dT%H:%M:%SZ", gmtime time);
 printf $logFH "ended at %s\n",$endtime if $debugging; 		#only do this if desired/requested
 #ToDo:  calculate elapsed time and add to logfile
 
-    
 exit 0;
 
 sub usage() {  #provides help
